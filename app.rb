54427ab89d4c2f09259264a2c18c7de96a102e37
Andrey Izotov <izotov100@gmail.com>
2014-05-31 02:56:42 -0700

    added user info route

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 class GameRecommender < Sinatra::Base
 	get '/' do
-		"Hello Sinatra!!!"
-	end
-
-	get '/test' do
 		content_type 'html'
 		erb :index
+	end
+
+	get '/user_info' do
+		content_type :json
+		{
+			username: 'username@domain.com',
+			other_things: 'other things go here'
+		}.to_json
 	end
 end
--- config.ru
+++ config.ru
@@ -4,6 +4,7 @@ require 'thin'
 require 'sass'
 require 'sass/plugin/rack'
 require './app.rb'
+require 'json'
 
 Sass::Plugin.options[:style] = :compressed
 use Sass::Plugin::Rack
